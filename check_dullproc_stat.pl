248a9d721cd442b13e259541725cbc4730474f7f
Alexander A. Klimov <alexander.klimov@icinga.com>
2021-10-25 12:19:01 +0200

    Add check_dullproc_stat.pl

---
+++ check_dullproc_stat.pl
@@ -0,0 +1,26 @@
+#!/usr/bin/env perl
+
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+
+use warnings;
+use strict;
+use autodie;
+
+print '|';
+
+local @ARGV = ('/proc/stat');
+
+while (<>) {
+	next unless /^(\w+)\s+(\d.*\d)/;
+	
+	my $subject = $1;
+	my $i = 0;
+
+	foreach ($2 =~ /\d+/g) {
+		++$i;
+		print " ${subject}_${i}=$_"
+	}
+}
